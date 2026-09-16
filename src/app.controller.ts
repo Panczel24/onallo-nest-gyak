d6bb964fc17bc146419b4c6f1940248fd3bef904
Panczel24 <pcsenge8@gmail.com>
2026-09-16 17:12:43 +0200

    itthoni próbálkozás

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,28 +3,55 @@ import { AppService } from './app.service.js';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   @Render('index')
   getHello() {
-    return{
-      szoveg:"Első önálló gyakorlás"
+    return {
+      szoveg: "Első önálló gyakorlás"
+
     }
   }
 
-  @Get()
+  @Get('bekezdesek')
   @Render('bekezdesek')
   getBekezdes() {
-    
-      const bekezdesek: string[] =[
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni neque autem saepe omnis voluptatibus quam exercitationem ab a cupiditate, repellat mollitia odio quasi sint hic, inventore deserunt dolorem laudantium voluptas"
-      ]
-    return{
+
+    const bekezdesek: string[] = [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum.",
+
+      "Cras venenatis euismod malesuada. Nullam ac erat ante. Sed ut perspiciatis unde omnis iste natus error sit voluptatem.",
+
+      "Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt."
+    ]
+    return {
       bekezdesek
     }
   }
 
+  @Get('red-blue')
+  @Render('red-blue')
+  getRedBlue() {
+    let szin = "";
+    const szam = Math.random();
+    if(szam<0.5){
+      szin = "red";
+    }else{
+      szin = "blue"
+    }
+
+    return {
+      szin
+    };
 
 
+
+
+
+
+
+
+  }
 }
+
